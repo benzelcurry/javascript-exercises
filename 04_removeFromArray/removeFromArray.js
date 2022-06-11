4a2949a63bbb50bd1861215b8cf37d14513c27c5
Ben <benawhiting@gmail.com>
2022-06-11 13:36:00 -0700

    Complete js exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(theArray, ...theArgs) {
+    /* for (let i = 0; i < theArray.length; i++) {
+         if (theArray[i] == theArgs) {
+            delete theArray[i];
+        } else {
+            continue;
+        }
+    } */
 
+    return theArray.filter(x => !theArgs.includes(x));
 };
 
 // Do not edit below this line
